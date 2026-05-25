a14508b5d41e7080460986461fe75a9931ae4a78
James Tognola <james.tognola@qdeveloping.co.uk>
2026-05-25 20:37:14 +0100

    Closing and opening of profile.

--- src/components/Profile/profile.tsx
+++ src/components/Profile/profile.tsx
@@ -1,26 +1,16 @@
 import "./profile.scss";
-import {getCollection} from "astro:content";
+import {useState} from "react";
 
-type IBlogPostProps = {};
+const Icon = () => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="#222" stroke-miterlimit="10" stroke-width="0" d="M254.307 69.55a9.8 9.8 0 0 0-2.88-6.956c-3.84-3.84-10.069-3.84-13.907 0l-109.663 109.66-109.66-109.66c-3.842-3.84-10.069-3.84-13.91 0-3.839 3.842-3.839 10.069 0 13.91l116.615 116.615c3.838 3.841 10.068 3.841 13.907 0L251.424 76.504a9.8 9.8 0 0 0 2.883-6.955"/></svg>;
 
-interface Post {
-    date: Date;
-    title: string;
-    link: string;
-}
+export const Profile = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-const ListItem = ({title, link, date}: Post) => {
-    return <li><a href={link}>{title}</a></li>;
-};
+    return <div className={"profile"}>
+        <h2 className={isOpen ? "is-open" : ""} onClick={() => setIsOpen(o => !o)}>Profile <Icon /></h2>
 
-// Server side.
-const POSTS =
-    (await getCollection('blog'))
-        .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-        .map(p => ({date: p.data.pubDate, title: p.data.title, link: `/blog/${p.id}`}));
-
-export const Profile = (props: IBlogPostProps) => {
-    return <div>
-        <h2>Profile</h2>
+        {isOpen && <div className="info">
+            <p>test</p>
+        </div>}
     </div>;
 }
