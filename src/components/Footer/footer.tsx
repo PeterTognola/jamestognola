6e70ca78c8f1586abc82d8c1468f456e2c6f779f
James Tognola <togdem1@googlemail.com>
2026-05-27 22:41:34 +0100

    Updating footer.

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,25 +1,10 @@
 import "./footer.scss";
 import {useState} from "react";
 
-interface IMenuItem {
-    link: string;
-    title: string;
-}
-
-const MenuItem = (item: IMenuItem) => {
-    return <li><a href={item.link}>{item.title}</a></li>;
-}
-
 export const Footer = () => {
-    const items: IMenuItem[] = [
-        {title: "Home", link: "/" },
-        {title: "Projects", link: "/projects" },
-        {title: "Posts", link: "/blog" },
-    ];
-
     const [date,] = useState<Date>(new Date());
 
     return <footer>
-        &copy; {date.getFullYear()} Your name here. All rights reserved.
+        <p>&copy; {date.getFullYear()} James Tognola. All rights reserved.</p>
     </footer>;
 }
