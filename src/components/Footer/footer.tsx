d4191d52f4836c06a9f5e30f6a4b2a74d1e74a4b
James Tognola <togdem1@googlemail.com>
2026-06-01 22:19:23 +0100

    Fixed styling and started updating the homepage.

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -5,6 +5,7 @@ export const Footer = () => {
     const [date,] = useState<Date>(new Date());
 
     return <footer>
+        <p>Disclaimer: The opinions expressed herein are my own personal opinions and do not represent my employer's view in any way.</p>
         <p>&copy; {date.getFullYear()} James Tognola. All rights reserved.</p>
     </footer>;
 }
