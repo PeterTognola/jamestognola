cbc47e7d59d96fc02bc3ee7646322a3270500334
James Tognola <togdem1@googlemail.com>
2026-06-03 14:55:34 +0100

    Refactoring repeated styles, moving some elements to constants and updating homepage.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import type {IMenuItem} from "./components/Header/header.tsx";
+
 export const SITE_TITLE = "James Tognola";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const MENU_ITEMS: IMenuItem[] = [
+    {title: "Home", link: "/"},
+    {title: "Projects", link: "/projects"},
+    {title: "Posts", link: "/blog"},
+];
+
+export const LOGO_INITIALS: string = "JT";
+
+export const GITHUB_LINK: string = "https://github.com/petertognola";
